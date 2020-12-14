a86730d81a3aabe4582b61defd56bc68937e6490
Michael <michaelbubel.1989@gmail.com>
2020-12-13 18:58:12 -0800

    Added the notes route

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ let PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Goes to the notes
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
 // Returns to the homepage
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/index.html"));
